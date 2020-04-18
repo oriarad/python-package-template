d6eecb06db8842a9539eef268a66759fe18ea518
Ori Arad <aradori@hotmail.com>
2020-04-18 22:48:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mypackage',
+      version='0.1',
+      description='A description of mypackage',
+      url='http://github.com/oriarad/python-package-template',
+      author='Ori Arad',
+      author_email='aradori@hotmail.com',
+      license='MIT',
+      packages=['mypackage'],
+      zip_safe=False)
